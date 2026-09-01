0b0503959d7a46f9e381c8cd3a0adbc25b9fa244
Mehran Akbarpour Ghazani <mehran.akbarpour.gh@gmail.com>
2026-09-01 19:13:40 +1000

    Make top and side nav

---
+++ scripts/myFunction.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var hMenu = document.getElementById("myLinks");
+  if (hMenu.style.display === "block") {
+    hMenu.style.display = "none";
+  } else {
+    hMenu.style.display = "block";
+  }
+}
