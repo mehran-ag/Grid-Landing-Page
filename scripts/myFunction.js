ac5d2fe89eca837f95745f40099a4b3c86e97921
Mehran Akbarpour Ghazani <mehran.akbarpour.gh@gmail.com>
2026-09-05 14:48:41 +1000

    Update background color

--- scripts/myFunction.js
+++ scripts/myFunction.js
@@ -1,8 +1,12 @@
 function myFunction() {
   var hMenu = document.getElementById("menuItems");
+  const containerElement = document.querySelector('.body-container');
   if (hMenu.style.display === "block") {
     hMenu.style.display = "none";
+    containerElement.classList.remove('blue-bg');
   } else {
     hMenu.style.display = "block";
+    containerElement.classList.add('blue-bg');
   }
+
 }
