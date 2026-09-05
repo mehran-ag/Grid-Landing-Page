00e39cc3f9a5fd2f90f3e8b02eb16556e7fa9a51
Mehran Akbarpour Ghazani <mehran.akbarpour.gh@gmail.com>
2026-09-05 21:25:06 +1000

    Update files with the new design (without bootstrap)

--- scripts/myFunction.js
+++ scripts/myFunction.js
@@ -1,12 +1,11 @@
 function myFunction() {
-  var hMenu = document.getElementById("menuItems");
-  const containerElement = document.querySelector('.body-container');
-  if (hMenu.style.display === "block") {
+  const hMenu = document.getElementById("menu-items-container");
+  const containerElement = document.querySelector('.main-body-container');
+  if (hMenu.style.display === "flex") {
     hMenu.style.display = "none";
     containerElement.classList.remove('blue-bg');
   } else {
-    hMenu.style.display = "block";
+    hMenu.style.display = "flex";
     containerElement.classList.add('blue-bg');
   }
-
 }
